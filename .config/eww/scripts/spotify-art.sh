79b660ddfc2ae9e828dcf048b10d80109418ff52
David <dawoud2003@gmail.com>
2026-01-05 20:31:05 +0100

    started work on spotify player

--- .config/eww/scripts/spotify-art.sh
+++ .config/eww/scripts/spotify-art.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-COVER_URL=$(playerctl metadata mpris:artUrl)
+TITLE=$(playerctl metadata --format {{album}} -p spotify)
+SAFE_TITLE=$(printf "%s" "$TITLE" | tr '/ ' '__')
+URL=$(playerctl metadata mpris:artUrl -p spotify)
+IMG_PATH="/tmp/$SAFE_TITLE-art.png"
 
-curl -s "$COVER_URL" -o /tmp/album-art.png
+curl -s "$URL" -o "$IMG_PATH"
+echo $IMG_PATH
